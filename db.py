e1077f32b58cfeea64b17f884ba05d6818483de5
Jorge Junior <jorgejunior@MacBook-Air-de-User.local>
2019-06-11 19:42:58 -0300

    empresa class

--- db.py
+++ db.py
@@ -1,4 +1,15 @@
 import sqlalchemy as db
-engine = db.create_engine('mysql://u159087763_mecon:zVMjhZWw5FeV@sql157.main-hosting.eu:3306')
-connection = engine.connect()
-print(connection)
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Empresa(Base):
+    __tablename__ = "Empresa"
+
+    id = Column('id', Integer, primary_key=True)
+    nome_fantasia = Column('fantasy_name', String, unique=True)
+
+
+engine = create_engine('mysql://u159087763_mecon:zVMjhZWw5FeV@sql157.main-hosting.eu:3306', echo=True)
+Base.metadata.create_all(bind=engine)
